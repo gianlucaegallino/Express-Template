b053ce54e91affff04efaab585f4b3cef998145e
giangallino <99291206+gianlucaegallino@users.noreply.github.com>
2025-02-02 10:32:28 -0300

    Require validator

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const path = require("node:path");
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 const assetsPath = path.join(__dirname, "public");
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,7 +9,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
 
 app.get("/", (req, res) => {
   res.render("index", {});
